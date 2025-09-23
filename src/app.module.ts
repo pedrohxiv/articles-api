1384cd7c1b43ae53c0efb25e64a252d6858fd4e3
Pedro <pedrohalmeidamendonca@gmail.com>
2025-09-23 17:39:26 -0300

    Creating tag resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { TagModule } from '@/tag/tag.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TagModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/tag/tag.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+import { TagController } from '@/tag/tag.controller';
+import { TagService } from '@/tag/tag.service';
+
+@Module({
+  controllers: [TagController],
+  providers: [TagService],
+})
+export class TagModule {}
